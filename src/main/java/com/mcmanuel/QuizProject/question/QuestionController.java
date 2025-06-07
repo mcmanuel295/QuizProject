30049fab62e77ea766af7856d7cf38e42135e9d8
unknown <mcmanuel755@gmail.com>
2025-06-07 14:38:14 +0100

    new commit

--- src/main/java/com/mcmanuel/QuizProject/question/QuestionController.java
+++ src/main/java/com/mcmanuel/QuizProject/question/QuestionController.java
@@ -1,4 +1,10 @@
 package com.mcmanuel.QuizProject.question;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequiredArgsConstructor
 public class QuestionController {
 }
