87180192d380e64c65937e1a03a24e6289e89002
unknown <mcmanuel755@gmail.com>
2025-06-08 01:55:48 +0100

    edited the quiz service and cotroller

--- src/main/java/com/mcmanuel/QuizProject/quiz/QuizServiceImp.java
+++ src/main/java/com/mcmanuel/QuizProject/quiz/QuizServiceImp.java
@@ -17,7 +17,6 @@ public class QuizServiceImp implements  QuizService{
         List<QuestionDto> questionDtoList = questionRepo.findRandomQuestionsByCategory(category,noOfQues).;
       return Quiz.builder()
               .title(title)
-              .
               .build();
     }
 }
