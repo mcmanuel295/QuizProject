97d75e003e818e06f70e9eb9e47ec780bf5bec4a
unknown <mcmanuel755@gmail.com>
2025-06-08 01:42:36 +0100

    edited the quiz service and cotroller

--- src/main/java/com/mcmanuel/QuizProject/quiz/QuizController.java
+++ src/main/java/com/mcmanuel/QuizProject/quiz/QuizController.java
@@ -1,6 +1,5 @@
 package com.mcmanuel.QuizProject.quiz;
 
-import com.mcmanuel.QuizProject.DifficultyLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
